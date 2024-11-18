42765acec69678388c7fbc1b4c90817ba39b8518
satyaki-das-usc <satyakid@usc.edu>
2024-11-18 12:51:14 -0800

    add caching options for stage 2 in run_abstract_dataflow.sh

--- DDFA/scripts/run_abstract_dataflow.sh
+++ DDFA/scripts/run_abstract_dataflow.sh
@@ -15,4 +15,6 @@ PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 8 --
 PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 16 --no-cache --stage 1 $@
 
 PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 8 --no-cache --stage 2 --sample $@
+PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 8 --cache --stage 2 --sample $@
 PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 16 --no-cache --stage 2 $@
+PYTHONPATH="." python -u sastvd/scripts/abstract_dataflow_full.py --workers 16 --cache --stage 2 $@
