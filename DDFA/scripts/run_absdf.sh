65f301d2b213ee805aa54618af575d5cf2dadf5d
Satyaki Das <satyakid@usc.edu>
2024-11-18 20:13:24 -0800

    add sample option to run_absdf.sh

--- DDFA/scripts/run_absdf.sh
+++ DDFA/scripts/run_absdf.sh
@@ -10,3 +10,5 @@
 source activate.sh
 
 python -u sastvd/scripts/dbize_absdf.py $@
+PYTHONPATH="." python -u sastvd/scripts/dbize_absdf.py --sample
+PYTHONPATH="." python -u sastvd/scripts/dbize_absdf.py
